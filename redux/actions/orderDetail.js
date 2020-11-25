70b087ff456afb144be015aef59645b6ea6272a5
kawikarob <64801223+kawikarob@users.noreply.github.com>
2020-11-24 20:12:04 -0800

    added redux orderDetail

---
+++ redux/actions/orderDetail.js
@@ -0,0 +1,43 @@
+import api from "../../utils/api";
+
+import {
+  FETCH_ORDER_DETAIL_ERROR,
+  FETCH_ORDER_DETAIL_REQUEST,
+  FETCH_ORDER_DETAIL_SUCCESS,
+} from "../types/orderDetail";
+
+export const fetchOrderDetailRequest = () => {
+  return {
+    type: FETCH_ORDER_DETAIL_REQUEST,
+  };
+};
+
+export const fetchOrderDetailSuccess = (data) => {
+  return {
+    type: FETCH_ORDER_DETAIL_SUCCESS,
+    payload: data,
+  };
+};
+
+export const fetchOrderDetailError = (error) => {
+  return {
+    type: FETCH_ORDER_DETAIL_ERROR,
+    payload: error,
+  };
+};
+
+export const fetchOrderDetail = (id) => {
+  return (dispatch) => {
+    dispatch(fetchOrderDetailRequest);
+    api
+      .get("orderDetail/" + id)
+      .then((response) => {
+        const data = response.data;
+        dispatch(fetchOrderDetailSuccess(data));
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        dispatch(fetchOrderDetailError(errorMsg));
+      });
+  };
+};
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import byId from "./byId";
 import auth from "./auth";
+import orderDeatil from "./orderDetail";
 export default combineReducers({
   auth: auth,
   byId: byId,
+  orderDeatil: orderDeatil,
 });
