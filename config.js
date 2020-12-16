494c9877e123febaa54c30e04924a3cf6a67300d
Tom <tom@omnitotaltech.com>
2020-12-16 13:44:39 -0600

    config

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 //export const APIAddress = "http://localhost:3000/api/v1/";
-export const APIAddress = "http://192.168.1.158:8080/api/v1/";
+export const APIAddress = "http://localhost:8080/api/v1/";
+//export const APIAddress = "http://192.168.1.158:8080/api/v1/";
