9539d35ea5c1af3c12a7a301964f5d31c1af1667
Tom <tom@omnitotaltech.com>
2020-12-23 20:25:05 -0600

    push

--- Screens/Home.js
+++ Screens/Home.js
@@ -16,6 +16,7 @@ import PlusImage from "../assets/images/plus-1.jpg";
 import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import DeliveryStatusIcon from "../Components/DeliveryStatusIcon";
+import { connect } from "react-redux";
 
 import Navbar from "../Components/Navbar";
 
@@ -73,75 +74,92 @@ const admin = [
   // },
 ];
 
-function Home(props) {
-  return (
-    <ScrollView>
-      <View style={{ backgroundColor: " #718096" }}>
-        <div className="h-screen ">
-          {/* Employee Area */}
-          <div className="mt-8">
-            <div className="container max-w-4xl w-full bg-white  rounded-lg shadow-lg mx-auto px-2">
-              <div className="text-3xl mx-4 ">
-                <span className="bg-white mb-4 m-0">Employee</span>
+class Home extends Component {
+  render() {
+    return (
+      <ScrollView>
+        {this.props.user.hasSignedInBefore ? (
+          <View style={{ backgroundColor: " #718096" }}>
+            <div className="h-screen ">
+              {/* Employee Area */}
+              <div className="mt-8">
+                <div className="container max-w-4xl w-full bg-white  rounded-lg shadow-lg mx-auto px-2">
+                  <div className="text-3xl mx-4 ">
+                    <span className="bg-white mb-4 m-0">Employee</span>
+                  </div>
+                  <FlatList
+                    horizontal
+                    data={user}
+                    renderItem={({ item: deliveryTypeIcon }) => {
+                      return (
+                        <TouchableOpacity
+                          onPress={() =>
+                            this.props.navigation.navigate(
+                              deliveryTypeIcon.location
+                            )
+                          }
+                          style={{ margin: "10px" }}
+                        >
+                          <DeliveryStatusIcon
+                            icon={deliveryTypeIcon.icon}
+                            status={deliveryTypeIcon.status}
+                            subtext={deliveryTypeIcon.subtext}
+                          />
+                        </TouchableOpacity>
+                      );
+                    }}
+                    keyExtractor={(item, index) => index}
+                  />
+                </div>
               </div>
-              <FlatList
-                horizontal
-                data={user}
-                renderItem={({ item: deliveryTypeIcon }) => {
-                  return (
-                    <TouchableOpacity
-                      onPress={() =>
-                        props.navigation.navigate(deliveryTypeIcon.location)
-                      }
-                      style={{ margin: "10px" }}
-                    >
-                      <DeliveryStatusIcon
-                        icon={deliveryTypeIcon.icon}
-                        status={deliveryTypeIcon.status}
-                        subtext={deliveryTypeIcon.subtext}
-                      />
-                    </TouchableOpacity>
-                  );
-                }}
-                keyExtractor={(item, index) => index}
-              />
+              {/* // Admin area */}
+              <div className="mt-8">
+                <div className="container max-w-4xl w-full bg-white  rounded-lg shadow-lg mx-auto px-2">
+                  <div className="text-3xl mx-4 ">
+                    <span className="bg-white mb-4 m-0">Admin</span>
+                  </div>
+                  <div className="container  max-w-4xl w-full bg-white  rounded-lg  px-2">
+                    <FlatList
+                      horizontal
+                      data={admin}
+                      renderItem={({ item: deliveryTypeIcon }) => {
+                        return (
+                          <TouchableOpacity
+                            onPress={() =>
+                              this.props.navigation.navigate(
+                                deliveryTypeIcon.location
+                              )
+                            }
+                            style={{ margin: "10px" }}
+                          >
+                            <DeliveryStatusIcon
+                              icon={deliveryTypeIcon.icon}
+                              status={deliveryTypeIcon.status}
+                              subtext={deliveryTypeIcon.subtext}
+                            />
+                          </TouchableOpacity>
+                        );
+                      }}
+                      keyExtractor={(item, index) => index}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
+          </View>
+        ) : (
+          <div className="container mx-auto">
+            they have not signed in before
           </div>
-          {/* // Admin area */}
-          <div className="mt-8">
-            <div className="container max-w-4xl w-full bg-white  rounded-lg shadow-lg mx-auto px-2">
-              <div className="text-3xl mx-4 ">
-                <span className="bg-white mb-4 m-0">Admin</span>
-              </div>
-              <div className="container  max-w-4xl w-full bg-white  rounded-lg  px-2">
-                <FlatList
-                  horizontal
-                  data={admin}
-                  renderItem={({ item: deliveryTypeIcon }) => {
-                    return (
-                      <TouchableOpacity
-                        onPress={() =>
-                          props.navigation.navigate(deliveryTypeIcon.location)
-                        }
-                        style={{ margin: "10px" }}
-                      >
-                        <DeliveryStatusIcon
-                          icon={deliveryTypeIcon.icon}
-                          status={deliveryTypeIcon.status}
-                          subtext={deliveryTypeIcon.subtext}
-                        />
-                      </TouchableOpacity>
-                    );
-                  }}
-                  keyExtractor={(item, index) => index}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </View>
-    </ScrollView>
-  );
+        )}
+      </ScrollView>
+    );
+  }
 }
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.user,
+  };
+};
 
-export default Home;
+export default connect(mapStateToProps)(Home);
