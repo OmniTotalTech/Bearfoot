cde7c3b8e498e0421842b55425c6ad27df41e802
Tom <tom@omnitotaltech.com>
2021-01-07 11:35:54 -0600

    change for prod

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 //export const APIAddress = "http://localhost:8080/api/v1/";
-export const APIAddress = "http://3.22.188.74:8080/api/v1/";
+export const APIAddress = "https://3.22.188.74:8080/api/v1/";
