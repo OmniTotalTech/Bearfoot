96513a028b5db8a0b81375d3cb8335c008fcd268
Tom <tom@omnitotaltech.com>
2021-01-07 14:51:58 -0600

    dev

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const APIAddress = "http://localhost:8080/api/v1/";
+// export const APIAddress = "http://localhost:8080/api/v1/";
 // export const APIAddress = "https://3.22.188.74:8080/api/v1/";
-//export const APIAddress = "https://api.bearfootapp.com/api/v1/";
+export const APIAddress = "https://api.bearfootapp.com/api/v1/";
