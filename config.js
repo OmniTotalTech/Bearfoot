0c405b8dfb9613eb8e4832829e75c1da62e6ab31
Tom <tom@omnitotaltech.com>
2021-01-20 14:51:15 -0600

    changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // export const APIAddress = "http://localhost:8080/api/v1/";
 // export const APIAddress = "https://3.22.188.74:8080/api/v1/";
 export const APIAddress =
-  "http://bfmtv2-1648336912.us-east-2.elb.amazonaws.com/api/v1/";
+  "https://bfmtv2-1648336912.us-east-2.elb.amazonaws.com/api/v1/";
