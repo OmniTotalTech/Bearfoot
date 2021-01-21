ac6d0c646cd1e33a3740a7ba9daa74684dd93a07
kawikarob <64801223+kawikarob@users.noreply.github.com>
2021-01-20 20:11:30 -0800

    styled checklist

--- Components/PoolInventoryCheckList.js
+++ Components/PoolInventoryCheckList.js
@@ -8,33 +8,33 @@ export default class PoolInventoryCheckList extends Component {
       console.log(this.state);
     };
     return (
-      <div className="container mx-auto h-full overflow-scroll">
-        <div className="w-full border-b-2 py-4">
+      <div className="container mx-auto h-full overflow-scroll bg-gray-100">
+        <div className="w-full shadow-lg border-b-2 py-4">
           <p className="text-3xl text-center my-4">
             Currently performing a {this.props.type} Checklist count.
           </p>
-          <p className="text-2xl">Last submitted report:</p>
+          <p className="text-2xl px-4">Last submitted report:</p>
           <p></p>
           {this.props.data.inventoryList ? (
             this.props.data.inventoryList.map((item, i) => (
               <>
-                <div key={i} className="px-2 w-3/4 text-md">
+                <div key={i} className="px-6 w-3/4 text-md">
                   {i + 1}. {item.name}
                 </div>
-                <div className="px-2 text-sm">{item.desc}</div>
-                <div className="px-2 text-sm">
+                <div className="px-6 text-sm">{item.desc}</div>
+                <div className="px-6 text-sm">
                   {item.unitType != "" ? (
                     "( " + item.unitType + " )"
                   ) : (
                     <div></div>
                   )}
                 </div>
-                <p className="text-sm px-2 m-2">Actual Count:</p>
-                <p className="text-md px-2 m-2"></p>
+                <p className="text-sm px-6">Actual Count:</p>
+                <p className="text-md px-6 m-2"></p>
                 <div className="w-full">
                   <input
                     onChange={(e, value) => handleChange(e, item._id)}
-                    className="text-right mx-2 p-2"
+                    className="text-right mx-6 mb-4 px-4 py-2 border-solid border-2 "
                     type="number"
                   />
                 </div>
@@ -44,6 +44,11 @@ export default class PoolInventoryCheckList extends Component {
           ) : (
             <div></div>
           )}
+          <div className="text-center pt-4">
+            <button className="bg-red-700 hover:bg-red-600 text-white font-semibold py-2 px-4 border border-red-400 rounded shadow">
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     );
